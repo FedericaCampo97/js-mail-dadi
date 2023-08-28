de84af74bcf5dfcb97d6ea5563879bc7e471e8d4
FedericaCampo97 <federica.campo97@gmail.com>
2023-08-28 17:10:28 +0200

    Complete dice game

--- assets/main.js
+++ assets/main.js
@@ -13,8 +13,17 @@ che "cosa" ci serve?*/
 
 //Dichiaro il numero del giocatore
 const numberGamer = Math.floor(Math.random() * 6) + 1;
-console.log ('Il numero del giocatore è ' + numberGamer)
+console.log ('Il numero del giocatore è ' + numberGamer);
 
 //Dichiaro il numero del computer
 const numberPC = Math.floor(Math.random() * 6) + 1;
-console.log ('Il numero del computer è ' + numberPC)
+console.log ('Il numero del computer è ' + numberPC);
+
+//Stabilisco il vincitore in base a chi fa il numero più alto 
+if(numberGamer > numberPC) {
+    console.log ('Il vincitore è il giocatore!');
+} else if(numberPC > numberGamer) {
+    console.log ('Il vincitore è il PC');
+} else if (numberGamer == numberPC) {
+    console.log ('Pareggio');
+}
